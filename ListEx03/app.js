3e282e99bac4c35e23c1870427679cdc5a6f2653
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2023-10-22 20:51:13 -0300

    adicionando sistema de Editar dados

--- ListEx03/app.js
+++ ListEx03/app.js
@@ -44,6 +44,37 @@ app.post("/insertUser", (req, res) => {
     })
 })
 
+app.get("/editUser/:id", (req, res) => {
+    const id = req.params.id
+    connection.query(
+        "select * from user where user_id = ?", [id],
+        (err, result) => {
+            if (err) {
+                console.log("Erro ao buscar registro", err)
+                return
+            } else {
+                res.render("editUser", {user: result[0]})
+            }
+        }
+    )
+})
+
+app.post("/editUser/:id", (req, res) => {
+    const id = req.params.id
+    const { name } = req.body
+    connection.query(
+        "update user set user_name = ? where user_id = ?", [name, id],
+        (err, result) => {
+            if (err) {
+                console.log("Erro ao editar registro", err)
+                return
+            } else {
+                res.redirect("/")
+            }
+        }
+    )
+})
+
 app.get("/deleteUser/:id", (req,res) => {
     const id = req.params.id
     connection.query(
@@ -67,9 +98,40 @@ app.get("/insertRequest", (req, res) => {
 app.post("/insertRequest", (req, res) => {
     const { id, name, desc, quantidade, preco } = req.body;
     connection.query(
-        "insert into purchase_request (user_id, request_name, request_desc, request_price, request_quantities) values (?, ?, ?, ?, ?)", [id, name, desc, quantidade, preco], (err, result) => {
+        "insert into purchase_request (user_id, request_name, request_desc, request_price, request_quantities) values (?, ?, ?, ?, ?)", [id, name, desc, preco, quantidade], (err, result) => {
             if (err) {
                 console.log(err)
+            } else {
+                res.redirect("/")
+            }
+        }
+    )
+})
+
+app.get("/editRequest/:id", (req, res) => {
+    const id = req.params.id
+    connection.query(
+        "select * from purchase_request where request_id = ?", [id],
+        (err, result) => {
+            if (err) {
+                console.log("Erro ao buscar registro", err)
+                return
+            } else {
+                res.render("editRequest", {request: result[0]})
+            }
+        }
+    )
+})
+
+app.post("/editRequest/:id", (req, res) => {
+    const id = req.params.id
+    const { name, desc, quantidade, preco, user_id } = req.body
+    connection.query(
+        "update purchase_request set user_id = ?, request_name = ?, request_desc = ?, request_price = ?, request_quantities = ? where request_id = ?", [user_id, name, desc, preco, quantidade, id],
+        (err, result) => {
+            if (err) {
+                console.log("Erro ao editar registro", err)
+                return
             } else {
                 res.redirect("/")
             }
